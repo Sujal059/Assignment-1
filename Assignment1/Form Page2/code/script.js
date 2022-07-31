42d258b571ce3e425716bd80059d28c97856a5cb
Sujal059 <96226592+Sujal059@users.noreply.github.com>
2022-07-31 23:01:08 +0530

    Add files via upload

---
+++ Assignment1/Form Page2/code/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("sidePanel").style.width = "200px";
+  }
+  
+  function closeNav() {
+    document.getElementById("sidePanel").style.width = "0";
+  }
